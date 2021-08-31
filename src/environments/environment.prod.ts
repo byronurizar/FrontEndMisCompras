2852fb7193761f12e84a3309fe49b4b96c33fcbe
Byron Lopez Urizar <blu.urizar@gmail>
2021-08-31 08:20:51 -0600

    Se agrego compilado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,8 +10,8 @@ export const environment = {
     appId: "1:346387311870:web:f726bf4f5f71480c6c09af",
     measurementId: "G-ZFR1FFNQ9W"
   },
-  urlBase:'https://api.natalysshop.com/apiStore/v1/',
-  urlImagnes:'https://api.natalysshop.com/'
+  urlBase:'http://api.natalysshop.com/apiStore/v1/',
+  urlImagnes:'http://api.natalysshop.com/'
   // urlBase:' http://127.0.0.1:3333/apiStore/v1/',
   //  urlImagnes:'http://127.0.0.1:3333/'
 };
